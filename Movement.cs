73a48ea362e3e8c345e1f14908c56fb129f7f80c
tanercinar <taner.cinar78@gmail.com>
2025-10-13 22:57:44 +0300

    Update Movement.cs

--- Movement.cs
+++ Movement.cs
@@ -2,34 +2,17 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    [Tooltip("Nesnenin hareket hızı (birim/saniye).")]
-    public float speed = 10f;
-
-    // Start is called before the first frame update
+    public float speed= 10f;
+    //private float speed2=10f;
     void Start()
     {
-        // Oyun başladığında nesnenin başlangıç pozisyonunu belirleyebiliriz.
-        // Örneğin: transform.position = new Vector3(0, 0.5f, 0);
-        Debug.Log("Hareket Kontrolü Başlatıldı!");
+        transform.position= new Vector3(0f, 5f, 0f);
     }
-
-    // Update is called once per frame
     void Update()
     {
-        // 1. Kullanıcı Girdisini Al
-        // Input.GetAxis, klavyedeki yön tuşları (veya WASD) girdilerini -1 ile 1 arasında yumuşak bir değer olarak alır.
-        float verticalInput = Input.GetAxis("Vertical");       // W, S, Yukarı/Aşağı Oklar
-        float horizontalInput = Input.GetAxis("Horizontal");   // A, D, Sol/Sağ Oklar
-
-        // 2. Hareket Vektörünü Oluştur
-        // Girdileri kullanarak hangi yöne hareket edileceğini belirleyen bir vektör oluşturuyoruz.
-        // Y eksenini 0 bırakıyoruz çünkü yukarı/aşağı bir hareket istemiyoruz.
-        Vector3 hareketYonu = new Vector3(horizontalInput, 0, verticalInput);
-
-        // 3. Hareketi Uygula
-        // transform.Translate, nesneyi mevcut konumundan belirtilen yöne doğru hareket ettirir.
-        // speed: Hareketi hızlandırmak için çarpan.
-        // Time.deltaTime: Hareketi kare hızından bağımsız hale getirerek saniyeye bağlı (akıcı ve tutarlı) yapar.
-        transform.Translate(hareketYonu * speed * Time.deltaTime);
+        float verticalInput= Input.GetAxis("Vertical");
+        float horizontalInput= Input.GetAxis("Horizontal");
+        Vector3 direction= new Vector3(horizontalInput, 0, verticalInput);
+        transform.position+= direction * speed * Time.deltaTime;
     }
 }
